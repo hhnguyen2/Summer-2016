3d9669cef4631aee2536f2a6cf068fa31965758b
Jimmy <hhnguyen@wesleyan.edu>
2016-06-06 15:24:40 -0700

    Add R files

---
+++ R/Binary-Newton-Raphson/newton_raphson_jun3.R
@@ -0,0 +1,117 @@
+## Define expit and dexpit
+# expit: e^x/(1+e^x)
+expit <- function(x){
+  exp(x)/(1+exp(x))
+}
+
+# dexpit: expit'(x), or expit(x)(1-expit(x))
+dexpit <- function(x){
+    expit(x)*(1 - expit(x))
+}
+
+## gen_my.data: Simulate data
+## Output: a list of 2 objects: 
+##      - $eta: original eta values used to generate simulation data
+##      - $sim.Matrix: n*p size matrix containing in order: zi, 1, xi
+gen_my.data <- function(x){
+    n <- 1000
+    p <- 4
+    prob <- 0.5
+    eta_0 <- 1
+    eta <- signif(runif(4,-3,3),digits = 2)  # Generate random eta between -3,3 with 2 significant digits
+    
+    randMatrix <- matrix(rbinom(n*p,1,prob),n,p) # x1, x2,...,xn
+    zi <- rbinom(n,1,expit(rowSums(sweep(randMatrix,MARGIN=2,eta,'*')) + eta_0)) # zi ~ expit(eta'*x + eta_0)
+    sim.Matrix <- cbind(zi,        # zi's
+                     rep(1,n),     # x0
+                     randMatrix)   # x1,x2,...,xn
+    
+    return(list(eta=c(eta_0,eta),sim.Matrix=sim.Matrix))
+}
+
+
+## Solver
+# Define function fr that evalutes f(eta)
+fr <- function(eta, sim.Matrix){
+    # INPUTS: eta, with form (eta_0, eta_1, ..., eta_n)
+    # OUTPUTS: f(eta) as column vector
+    # ----------------
+    # Extract x1 to xn
+    xi <- sim.Matrix[,3:6] # x1, ..., x4
+
+    # Evaluate f(eta); eta[1] is eta_0
+    zi.minus.pi <- sim.Matrix[,1] - expit(eta[1] + rowSums(sweep(xi,MARGIN=2,eta[2:length(eta)],'*'))) # zi - xi*eta
+    
+    # Output answer as column vector
+    as.matrix(colSums(sim.Matrix[,2:6] * zi.minus.pi))
+}
+
+# Define function grr that evalutes f'(eta)
+grr <- function(eta, sim.Matrix){
+    # INPUTS: eta, with form (eta_0, eta_1, ..., eta_n)
+    # OUTPUTS: Inverse Jacobian of f(eta) 
+    # ----------------
+    # Allocate empty answer matrix, should be 5x5
+    ans <- matrix(0,length(eta),length(eta))
+    ones.matrix <- matrix(1,length(eta),length(eta))
+    
+    # Iteratively generate Jacobian matrix and add to allocated matrix
+    for (i in 1:nrow(sim.Matrix)){
+        add.me <- sweep(ones.matrix,MARGIN=1,sim.Matrix[i,2:6],'*')
+        add.me <- sweep(add.me,MARGIN=2,sim.Matrix[i,2:6],'*')
+        add.me <- -1 * add.me * dexpit(eta[1] + sum(sim.Matrix[i,3:6] * eta[2:5]))
+        
+        ans <- ans + add.me
+    }
+    
+    # Evaluate inverse Jacobian. Return NA matrix if singular. 
+    tryCatch(solve(ans), error=function(e) matrix(NA,5,5))
+}
+
+# Newton Raphson to solve for eta
+newtonRaphson <- function(eta_0,real_eta,sim.Matrix){
+    # Initial setup
+    count <- 0
+    max_iterations <- 100
+    
+    # Set eta_t using eta_0 input, and then update eta_t.plus.one. 
+    eta_t <- eta_0
+    eta_t.plus.one <- eta_t - (grr(eta_t,sim.Matrix) %*% fr(eta_t,sim.Matrix))
+
+# Loop until squared diff between eta_t and eta_t.plus.one is minimized
+#      or maximum alloted iterations is reached
+    while(count < max_iterations &&
+          norm(matrix(eta_t.plus.one - eta_t), "F") > .Machine$double.eps){
+        # Increment counter
+        count <- count + 1
+        
+        # Update eta_t
+        eta_t <- eta_t.plus.one
+        eta_t.plus.one <- eta_t - (grr(eta_t,sim.Matrix) %*% fr(eta_t,sim.Matrix))
+    }
+    
+    #cat("Total iterations: ", count, "\n",
+    #    "Eta_t.plus.one: ", eta_t.plus.one, "\n",
+    #    "Real eta: ", real_eta, "\n",
+    #    "Squared eta diff ", norm(real_eta - eta_t.plus.one), "F")
+    norm(eta_t.plus.one - real_eta,"F")
+}
+
+#newtonRaphson(myEta,c(eta_0,eta),sim.Matrix)
+iter <- 750
+initial_eta <- c(0,0,0,0,0)
+sqr_diffs <- rep(0,iter)
+ptm <- proc.time() #let's time this slow code...
+
+for (i in 1:iter){
+    sim.data <- gen_my.data()
+    sqr_diffs[i] <- sqr_diffs[i] + newtonRaphson(initial_eta,sim.data$eta,sim.data$sim.Matrix)
+    
+    if (i %% 2 == 0)
+        print(i)
+}
+
+proc.time() - ptm
+hist(sqr_diffs,breaks=15)
+summary(sqr_diffs)
+cat("SD: ", sd(sqr_diffs), "\n")
